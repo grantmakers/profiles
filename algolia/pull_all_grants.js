982ef57231ccf6c4e6e4553d72ac3b4a0f6591e0
Chad Kruse <chadokruse@users.noreply.github.com>
2017-08-24 20:07:14 -0500

    Simplify mongo grants script and add _tags field

--- algolia/pull_all_grants.js
+++ algolia/pull_all_grants.js
@@ -1,7 +1,7 @@
 db.normalized.aggregate([  
   { 
     $match: {
-      //'normalized.ein': '582303901'
+      'normalized.ein': '521798711'
     }
   },
   {
@@ -16,15 +16,8 @@ db.normalized.aggregate([
       '_id': 0,
       'ein': '$normalized.ein',
       'organization_name': '$normalized.organization_name',
-      'assets': '$normalized.assets',
-      'website': '$normalized.website',
       'city': '$normalized.city',
       'state': '$normalized.state',
-      'has_website': '$normalized.has_website',
-      'grant_max': '$normalized.grant_max',
-      'grant_min': '$normalized.grant_min',
-      'grant_median': '$normalized.grant_median',
-      'grant_count': '$normalized.grant_count',
       'tax_year': '$normalized.tax_year',
       'grant_amount': '$normalized.grants.amount',
       'grant_purpose': '$normalized.grants.purpose',
@@ -32,12 +25,23 @@ db.normalized.aggregate([
       'grantee_city': '$normalized.grants.city',
       'grantee_state': '$normalized.grants.state',
       'index_for_objectID': 1,
+      '_tags': ['$normalized.ein'],
+      //'assets': '$normalized.assets',
+      //'website': '$normalized.website',
+      //'has_website': '$normalized.has_website',
+      //'grant_max': '$normalized.grant_max',
+      //'grant_min': '$normalized.grant_min',
+      //'grant_median': '$normalized.grant_median',
+      //'grant_count': '$normalized.grant_count',
     },
   }
 ]).forEach(function(x){
   const newID = x.ein + '_' + x.tax_year + '_' + x.index_for_objectID;
   x.objectID = newID;
   x._id = newID;
-  x.index_for_objectID = x.index_for_objectID + 1;
+  x.grant_number = x.index_for_objectID + 1;
+  delete x.index_for_objectID;
   db.grants.insert(x);
 });
+
+// mongoexport --db irs --collection grants --out grants.json --jsonArray
