fee88e57afb81add25b53e5b4d03ed138fe882b1
Chad Kruse <chadokruse@users.noreply.github.com>
2017-08-29 21:44:46 -0500

    Interim checkin

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,8 +1,30 @@
+---
+---
 // FAB Button
 // ===================
-/*
 (function() {
-  var fab = '<div class="fixed-action-btn"><a class="btn-floating btn-large btn-grantmakers waves-effect"><i class="material-icons">mail</i></a></div>';
-  $('.feedback-wrapper').html(fab);
+
+  // Styling 
+  // temporary until new css released across all profiles
+  var isMobile = window.matchMedia('only screen and (max-width: 992px)').matches;
+  var fabTempStyling;
+  if (!isMobile) {
+    fabTempStyling = '<style> .fixed-action-btn {right: 46px;}</style>';
+  }
+
+  // Modal
+  var modal = '{% include feedback.html %}'
+
+  // Button
+  var fab = '<div class="fixed-action-btn"><a class="waves-effect btn-floating btn-large btn-grantmakers modal-trigger" href="#feedback-js-modal"><i class="material-icons">mail</i></a></div>';
+
+  // Insert into DOM
+  //$('body').append(modal);
+  $('.feedback-wrapper').html(fabTempStyling + fab);
+
+  // Initialize
+  $('.modal').modal();
+  $('#feedback-js-modal').modal('open');
+  //$('.modal-trigger').modal();
+
 })();
-*/
