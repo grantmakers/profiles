2907a393805d5437af10d88c9ad4480dddc82872
Chad Kruse <chadokruse@users.noreply.github.com>
2017-09-19 20:24:00 -0500

    Improve handling of foreign grants + eslint fixes

--- algolia/pull_all_grants.js
+++ algolia/pull_all_grants.js
@@ -1,19 +1,19 @@
-db.normalized.aggregate([  
-  { 
-    $match: {
-      //'normalized.ein': '521798711'
-      //'normalized.ein': { $in: ['050509502', '521798711', '131684331'] }
-    }
+db.normalized.aggregate([
+  {
+    '$match': {
+      // 'normalized.ein': '521798711'
+      // 'normalized.ein': { $in: ['050509502', '521798711', '131684331'] }
+    },
   },
   {
-    $unwind: {
-      path: '$normalized.grants',
-      includeArrayIndex: 'index_for_objectID',
-      preserveNullAndEmptyArrays: false,
-    }
+    '$unwind': {
+      'path': '$normalized.grants',
+      'includeArrayIndex': 'index_for_objectID',
+      'preserveNullAndEmptyArrays': false,
+    },
   },
   {
-    $project: {
+    '$project': {
       '_id': 0,
       'ein': '$normalized.ein',
       'organization_name': '$normalized.organization_name',
@@ -25,15 +25,22 @@ db.normalized.aggregate([
       'grantee_name': '$normalized.grants.name',
       'grantee_city': '$normalized.grants.city',
       'grantee_state': '$normalized.grants.state',
+      'grantee_country': '$normalized.grants.country',
+      'grantee_is_foreign': '$normalized.grants.is_foreign',
       'index_for_objectID': 1,
     },
   }
-]).forEach(function(x){
+]).forEach(function(x) {
   let newID = x.ein + '_' + x.tax_year + '_' + x.index_for_objectID;
   x.objectID = newID;
   x._id = newID;
   x.grant_number = x.index_for_objectID + 1;
   delete x.index_for_objectID;
+  if (x.grantee_is_foreign) {
+    x.grantee_state_displayed = x.grantee_country + '*';
+  } else {
+    x.grantee_state_displayed = x.grantee_state;
+  }
   db.grants.insert(x);
 });
 
