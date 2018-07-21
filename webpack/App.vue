fb3892533415c4e2ca44996f6319242785e93469
Chad Kruse <chadokruse@users.noreply.github.com>
2018-07-21 14:02:31 -0500

    Initial commit - add Stitch to Vue + move insights to Vue

--- webpack/App.vue
+++ webpack/App.vue
@@ -8,23 +8,33 @@
     <SavedProfilesList
       :profiles="profiles"
     />
+    <Insights
+      :insights="insights"
+    />
   </div>
 </template>
 
 <script>
+import { Stitch, AnonymousCredential } from 'mongodb-stitch-browser-core';
 import ActionBar from './components/ActionBar';
 import SavedProfilesList from './components/SavedProfilesList';
+import Insights from './components/Insights';
 
 export default {
   components: {
     ActionBar,
     SavedProfilesList,
+    Insights,
   },
 
   data: function() {
     return {
       profiles: this.getListFromLocalStorage(),
       org: this.getCurrentOrgData(),
+      stitchClientObj: {},
+      stitchUserObj: {},
+      insights: [],
+      userData: {},
     };
   },
 
@@ -35,9 +45,46 @@ export default {
 
     const elems = document.querySelectorAll('.v-tooltipped');
     M.Tooltip.init(elems);
+
+    this.initializeStitchAndLogin();
   },
 
   methods: {
+    initializeStitchAndLogin: function() {
+      const client = Stitch.initializeDefaultAppClient('insights-xavlz');
+      client.auth.loginWithCredential(new AnonymousCredential())
+        .then(user => {
+          this.stitchClientObj = client;
+          this.stitchUserObj = user;
+          this.getInsightsFromStitch();
+          this.getUserDataFromStitch(user.id);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+
+    getUserDataFromStitch: function(id) {
+      this.stitchClientObj.callFunction('getUserData', [id])
+        .then(result => {
+          this.userData = result;
+        })
+        .catch(error => {
+          console.log('Error from calling getUserData function');
+          console.log(error);
+        });
+    },
+
+    getInsightsFromStitch: function() {
+      this.stitchClientObj.callFunction('getInsights', [this.org.ein])
+        .then(result => {
+          this.insights = result;
+        })
+        .catch(error => {
+          console.log('Error from calling getInsights function');
+        });
+    },
+    
     getListFromLocalStorage: function() {
       return JSON.parse(localStorage.getItem('profiles')) || [];
     },
---
+++ webpack/components/Insights.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul
+    v-if="insights.length"
+    class="collection"
+  >
+    <li
+      v-for="(item, index) in insights"
+      :key="index"
+      class="collection-item avatar"
+    >
+      <span class="title">{{ item.title }}</span><br>
+      {{ item.key }}
+      <p>{{ item.text }}</p>
+      <small><em>submitted by:</em> {{ item.author.full_name_displayed }}</small>
+    </li>
+    <li>
+      <hr class="slim">
+    </li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+  props: {
+    insights: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  mounted: function() {
+    // Render to insights element outside Vue app
+    document.getElementById('insights').prepend(this.$el);
+    // TODO Results in duplicates in Vue DevTools
+    // https://github.com/vuejs/vue-devtools/issues/645
+  },
+};
+</script>
