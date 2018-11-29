d956656440f2b59c241ce59ad65f5983b40117b8
Chad Kruse <chadokruse@users.noreply.github.com>
2018-11-29 16:09:03 -0600

    Simplify full index js

---
+++ assets/js/profile-index.js
@@ -0,0 +1,39 @@
+---
+---
+$(document).ready(function() {
+  'use strict';
+
+  // Navbar
+  // =======================================================
+  const header = $('.header');
+  const navbar = $('.navbar-profile');
+  const range = 64; // Height of navbar
+
+  $(window).on('scroll', function() {
+    let scrollTop = $(this).scrollTop();
+    let height = header.outerHeight();
+    let offset = height / 2;
+    let calc = 1 - (scrollTop - offset + range) / range;
+
+    header.css({ 'opacity': calc });
+
+    if (calc > '1') {
+      header.css({ 'opacity': 1 });
+      navbar.addClass('affix-top');
+      navbar.removeClass('affix');
+    } else if ( calc < '0' ) {
+      header.css({ 'opacity': 0 });
+      navbar.addClass('affix');
+      navbar.removeClass('affix-top');
+    }
+  });
+
+  // Materialize components
+  // =======================================================
+  window.onload = function() {
+    $('.tooltipped:not(.v-tooltipped)').tooltip(); // :not ensures Vue handles relevant initiation for Vue-controlled elements
+    $('.collapsible').collapsible({
+      'accordion': false,
+    });
+  };
+});
