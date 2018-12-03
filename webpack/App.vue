df5fc86a170fb0f34ec291c80bf32030aba39da6
Chad Kruse <chadokruse@users.noreply.github.com>
2018-12-03 09:25:28 -0600

    Improve reliability of Stitch init sequence

--- webpack/App.vue
+++ webpack/App.vue
@@ -83,10 +83,10 @@ export default {
       try {
         await this.stitchSetClient();
         await this.stitchLogin();
-        // TODO Improve readability of isLoggedIn protection
-        // Currently this happens in each Stitch function call below (e.g. is duplicative)
-        this.stitchGetInsights(0);
-        this.stitchGetUserData(0);
+        if (this.stitchClientObj.auth.isLoggedIn) {
+          this.stitchGetInsights(0);
+          this.stitchGetUserData(0);
+        }
       } catch (error) {
         bugsnagClient.notify(new Error('Stitch error - ' + error), {
           metaData: {'stitch': 'initializeStitchAndLogin'},
@@ -104,94 +104,80 @@ export default {
         bugsnagClient.notify(new Error('Stitch error - Second attempt at Stitch.init required - beforeCreate failed'), {
           metaData: {'stitch': 'stitchInit beforeCreate'},
         });
+        return this.stitchClientObj;
       } else {
         this.stitchClientObj = await Stitch.defaultAppClient;
+        return this.stitchClientObj;
       }
     },
 
-    stitchLogin: async function() {
+    stitchLogin: function() {
       if (!this.stitchClientObj.auth.isLoggedIn) {
         const credential = new AnonymousCredential();
-        await this.stitchClientObj.auth.loginWithCredential(credential)
+        return this.stitchClientObj.auth.loginWithCredential(credential)
           .catch(error => {
             bugsnagClient.notify(new Error('Stitch Login - ' + error), {
               metaData: {'stitch': 'stitchLogin'},
             });
           });
+      } else {
+        return true;
       }
     },
 
     stitchGetInsights: function(count) {
       let retryCount = count;
-      // Stitch functions return a promise
-      // TODO Move isLoggedIn protection to initializeStitchandLogin method for improved readability?
-      if (this.stitchClientObj.auth.isLoggedIn) {
-        this.stitchClientObj.callFunction('getInsights', [this.org.ein])
-          .then(result => {
-            this.insights = result;
-          })
-          .catch(error => {
-            // TODO DRY-up retry attempts
-            bugsnagClient.notify(new Error('Stitch getInsights - ' + error), {
-              metaData: {'stitch': 'stitchGetInsights'},
-            });
-            if (retryCount < 1) {
-              retryCount++;
-              this.stitchGetInsights(retryCount);
-            } else {
-              bugsnagClient.notify(new Error('Stitch getInsights retry - ' + error), {
-                metaData: {'stitch': 'stitchGetInsights retry'},
-              });
-            }
+      return this.stitchClientObj.callFunction('getInsights', [this.org.ein])
+        .then(result => {
+          this.insights = result;
+        })
+        .catch(error => {
+          // TODO DRY-up retry attempts
+          bugsnagClient.notify(new Error('Stitch getInsights - ' + error), {
+            metaData: {'stitch': 'stitchGetInsights'},
           });
-      } else {
-        bugsnagClient.notify(new Error('Stitch getInsights - Vue function reached but Stitch function not called'), {
-          metaData: {'stitch': 'stitchGetInsights called but not logged in'},
-          severity: 'info',
+          if (retryCount < 1) {
+            retryCount++;
+            this.stitchGetInsights(retryCount);
+          } else {
+            bugsnagClient.notify(new Error('Stitch getInsights retry - ' + error), {
+              metaData: {'stitch': 'stitchGetInsights retry'},
+            });
+          }
         });
-      }
     },
 
     stitchGetUserData: function(count) {
       let retryCount = count;
-      // Stitch functions return a promise
-      // TODO Move isLoggedIn protection to initializeStitchandLogin method for improved readability?
-      if (this.stitchClientObj.auth.isLoggedIn) {
-        this.stitchClientObj.callFunction('getUserData', [])
-          .then(result => {
-            if (result) {
-              this.profiles = result.profiles.sort(function(a, b) {
-                // Descending - last saved appears first
-                if (a.saved_on < b.saved_on) {
-                  return 1;
-                }
-                if (a.saved_on > b.saved_on) {
-                  return -1;
-                }
-                return 0;
-              });
-            }
-          })
-          .catch(error => {
-            // TODO DRY-up retry attempts
-            bugsnagClient.notify(new Error('Stitch getUserData - ' + error), {
-              metaData: {'stitch': 'stitchGetUserData'},
+      return this.stitchClientObj.callFunction('getUserData', [])
+        .then(result => {
+          if (result) {
+            this.profiles = result.profiles.sort(function(a, b) {
+              // Descending - last saved appears first
+              if (a.saved_on < b.saved_on) {
+                return 1;
+              }
+              if (a.saved_on > b.saved_on) {
+                return -1;
+              }
+              return 0;
             });
-            if (retryCount < 1) {
-              retryCount++;
-              this.stitchGetUserData(retryCount);
-            } else {
-              bugsnagClient.notify(new Error('Stitch getUserData retry - ' + error), {
-                metaData: {'stitch': 'stitchGetUserData retry'},
-              });
-            }
+          }
+        })
+        .catch(error => {
+          // TODO DRY-up retry attempts
+          bugsnagClient.notify(new Error('Stitch getUserData - ' + error), {
+            metaData: {'stitch': 'stitchGetUserData'},
           });
-      } else {
-        bugsnagClient.notify(new Error('Stitch getUserData - Vue function reached but Stitch function not called'), {
-          metaData: {'stitch': 'stitchGetUserData called but not logged in'},
-          severity: 'info',
+          if (retryCount < 1) {
+            retryCount++;
+            this.stitchGetUserData(retryCount);
+          } else {
+            bugsnagClient.notify(new Error('Stitch getUserData retry - ' + error), {
+              metaData: {'stitch': 'stitchGetUserData retry'},
+            });
+          }
         });
-      }
     },
 
     getCurrentOrgData: function() {
