a7be2fabf127300376c0e29cc8bd454f5405fffc
Chad Kruse <chadokruse@users.noreply.github.com>
2019-06-19 15:45:24 -0500

    Minor improvements to profiles search

--- assets/js/search.js
+++ assets/js/search.js
@@ -56,6 +56,7 @@ $(document).ready(function() {
   search.addWidget(
     instantsearch.widgets.searchBox({
       container: '#ais-widget-search-box',
+      placeholder: 'Search by recipient name, keyword, or location',
       poweredBy: false,
       autofocus: false,
       reset: true,
@@ -260,7 +261,7 @@ $(document).ready(function() {
       container: '#ais-widget-range-slider',
       attributeName: 'grant_amount',
       collapsible: {
-        collapsed: true,
+        collapsed: false,
       },
       cssClasses: {
         header: widgetHeaderClasses,
@@ -271,7 +272,7 @@ $(document).ready(function() {
       },
       tooltips: {
         format: function(rawValue) {
-          return '$' + Math.round(rawValue).toLocaleString();
+          return `$${numberHuman(rawValue, 0)}`;
         },
       },
       pips: false,
@@ -606,5 +607,18 @@ $(document).ready(function() {
       item.cssId = 'id-' + randomId();
     }
     return item;
+  }
+
+  function numberHuman(num, decimals) {
+    if (num === null) { return null; } // terminate early
+    if (num === 0) { return '0'; } // terminate early
+    if (isNaN(num)) { return num; } // terminate early if already a string - handles edge case likely caused by cacheing
+    const fixed = !decimals || decimals < 0 ? 0 : decimals; // number of decimal places to show
+    const b = num.toPrecision(2).split('e'); // get power
+    const k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3); // floor at decimals, ceiling at trillions
+    const c = k < 1 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3) ).toFixed(1 + fixed); // divide by power
+    const d = c < 0 ? c : Math.abs(c); // enforce -0 is 0
+    const e = d + ['', 'K', 'M', 'B', 'T'][k]; // append power
+    return e;
   }
 });
