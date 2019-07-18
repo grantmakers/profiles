cc4621dac1cdb7e19a3ab3658d8c9956b6d99675
Chad Kruse <chadokruse@users.noreply.github.com>
2019-07-18 08:59:48 -0500

    Quick linter cleanup

--- webpack/components/SavedProfilesList.vue
+++ webpack/components/SavedProfilesList.vue
@@ -5,12 +5,17 @@
   >
     <div class="modal-content">
       <div class="saved-profiles">
-        <div> <!-- on full page, use container-custom -->
+        <!-- on full page, use container-custom -->
+        <div>
           <div class="row">
-            <div class="col s12"> <!-- on full page, use l8 offset-l2 -->
-              <h5 class="left">Saved Profiles
+            <!-- on full page, use l8 offset-l2 -->
+            <div class="col s12">
+              <h5 class="left">
+                Saved Profiles
               </h5>
-              <p class="right">Visitor <i class="material-icons">account_circle</i></p>
+              <p class="right">
+                Visitor <i class="material-icons">account_circle</i>
+              </p>
             </div>
           </div>
           <div class="row">
@@ -25,7 +30,8 @@
                   />
                 </ul>
                 <div class="row">
-                  <div class="col s12"> <!-- on full page, use l8 offset-l2 -->
+                  <!-- on full page, use l8 offset-l2 -->
+                  <div class="col s12">
                     <p class="small text-muted">
                       CAUTION: If you reset your browser your saved profiles will be lost
                     </p>
