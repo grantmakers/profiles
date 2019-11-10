bab079fd54d3dc97a1ddc27d4446d3535a128006
Chad Kruse <chadokruse@users.noreply.github.com>
2019-11-10 13:28:58 -0600

    Ignore fbclid errors [refs #78]

--- webpack/utils/bugsnag.js
+++ webpack/utils/bugsnag.js
@@ -2,5 +2,13 @@ import bugsnag from 'bugsnag-js';
 const bugsnagClient = bugsnag({
   apiKey: '00805536ffe700bc0b6bf8452b547742', // Notify Key - is public
   appVersion: appVersion,
+  beforeSend: function(report) {
+    // Suppress unhandled errors caused by Facebook's fbclid parameter
+    const params = new URLSearchParams(location.search);
+    const fbclidCheck = params.get('fbclid');
+    if (fbclidCheck && report.errorClass === 'TypeError' && report.errorMessage === 'Illegal invocation') {
+      report.ignore();
+    }
+  },
 });
 export default bugsnagClient;
