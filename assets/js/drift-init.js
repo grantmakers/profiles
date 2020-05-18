03e10af755dfcfea522b20bf9c647ee6f5243443
Chad Kruse <chadokruse@users.noreply.github.com>
2020-05-18 11:34:57 -0500

    Remove jQuery from Drift setup

--- assets/js/drift-init.js
+++ assets/js/drift-init.js
@@ -1,6 +1,15 @@
 // Start of Async Drift Code
-$(document).ready(function() {
-  !function() {
+/* eslint-disable */
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(function() {
+  (!function() {
     var t;
     if (t = window.driftt = window.drift = window.driftt || [], !t.init) return t.invoked ? void (window.console && console.error && console.error("Drift snippet included twice.")) : (t.invoked = !0, 
     t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
@@ -17,15 +26,12 @@ $(document).ready(function() {
       o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + i + "/" + t + ".js", 
       n = document.getElementsByTagName("script")[0], n.parentNode.insertBefore(o, n);
     });
-  }();
+  })();
+
   drift.SNIPPET_VERSION = '0.3.1';
   drift.load('wmmk7c67hfg2');
-});
-// End of Async Drift Code -->
 
-// Start of Drift events for Google Analytics
-$(document).ready(function() {
-  drift.on('ready', function(api) {
+  drift.on('ready', function() {
     drift.on('welcomeMessage:open', function() {
       ga('send', 'event', {
         'eventCategory': 'Drift Widget',
@@ -49,4 +55,4 @@ $(document).ready(function() {
     });
   });
 });
-// End of Drift events for Google Analytics
+// End of Async Drift Code -->
