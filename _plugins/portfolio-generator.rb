4b321c1d7b1ec5f400831f51a93b1b9eb100d9c0
Chad Kruse <314653+chadokruse@users.noreply.github.com>
2024-12-07 15:09:29 -0600

    Add new Pub78-related redirects

--- _plugins/portfolio-generator.rb
+++ _plugins/portfolio-generator.rb
@@ -27,20 +27,6 @@ module Jekyll
         end
     end
 
-    class GrantsPage < Page
-        def initialize(site, base, dir, project_data)
-            @site = site
-            @base = base
-            @dir = dir
-            @name = "index.json"
-
-            self.process(@name)
-            self.read_yaml(File.join(base, "_layouts"), "grants.html")
-
-            project_data.each { |key, value| self.data[key] = value }
-        end
-    end
-
     class PortfolioGenerator < Generator
         safe true
 
@@ -60,21 +46,27 @@ module Jekyll
                 # Grantmakers.io url scheme
                 # /profiles/123456789-some-foundation
                 # EIN Redirect: /profiles/123456789
-                file_name_slug = slugify(project["ein"] + "-" + project["organization_name"])
+                file_name_slug = (project["ein"] + "-" + project["organization_name_slug"])
                 path = File.join(dir, file_name_slug)
                 project["dir"] = path
-
-                # Grants JSON: /profiles/123456789/grants
-                grants_path = File.join(project["ein"], "grants")
-
 
                 site.pages << ProjectPage.new(site, site.source, path, project) # Main profile link
                 site.pages << RedirectPage.new(site, site.source, project["ein"], project) # EIN redirect
 
-                # Create grants JSON api endpoint if > 50 current year grants
-                # if (project["grant_count"] > 50 )
-                #    site.pages << GrantsPage.new(site, site.source, grants_path, project)
-                # end
+                # Handle Pub78 name changes
+                if project["organization_name_legacy_slug_requires_redirect"] == true
+                    if project.has_key?("organization_name_legacy_slug")
+                        file_name_legacy_slug = (project["ein"] + "-" + project["organization_name_legacy_slug"])
+                        path_legacy_name = File.join(dir, file_name_legacy_slug)
+                        project["dir"] = path_legacy_name
+                        site.pages << RedirectPage.new(site, site.source, path_legacy_name, project)
+                    else 
+                        # Capture errors from missing legacy-slugs - this should never trigger
+                        puts "EIN: #{project["ein"].inspect}"
+                        puts "Org name slug: #{project["organization_name_slug"].inspect}"
+                        puts "Org name legacy slug: #{project["organization_name_legacy_slug"].inspect}"
+                        end
+                end
 
                 # Create redirects if name has changed
                 date_updated_grantmakers = DateTime.strptime(site.config["last_updated_grantmakers"], '%Y-%m-%dT%H:%M:%S.%N%z').to_s
@@ -132,74 +124,6 @@ module Jekyll
                     site.pages << RedirectPage.new(site, site.source, malformed_path, project)
                 end
             end
-        end
-
-        def compute_related_projects(site)
-            projects = []
-            site.data["ein"].each { |project| projects.push(project[1]) }
-
-            keys = site.config["related_project_keys"]
-
-            projects.each do |project|
-                project["related_projects"] = []
-                min = site.config["related_min_common"] || 0.6
-
-                projects_copy = []
-
-                projects.clone.each do |copy_project|
-                    projects_copy.push([copy_project, get_matches(project, copy_project, keys)])
-                end
-
-                related = projects_copy.keep_if { |project2| project2[1] >= min }
-                related = related.sort { |related_project, related_project2| related_project[1] <=> related_project2[1] }
-                related.reverse!.each { |related_project| project["related_projects"].push(related_project[0]) }
-            end
-        end
-
-        def get_matches(project1, project2, keys)
-            total = 0
-            total_possible_matches = get_total_possible_matches(project1, keys)
-
-            if project1.to_a == project2.to_a || total_possible_matches == 0
-                return total
-            else
-                keys.each { |key| total += get_num_matches_for_key(project1, project2, key) }
-            end
-
-            result = total.fdiv(total_possible_matches).round(2)
-
-            # Uncomment to see info about the matches for each project
-            #puts "Matches between #{project1["title"]} and #{project2["title"]}: #{total} (#{result})"
-
-            return result
-        end
-
-        def get_total_possible_matches(project, keys)
-            total = 0
-
-            keys.each do |key|
-                if project[key].class == String
-                    total += 1
-                else
-                    total += project[key].to_a.length()
-                end
-            end
-
-            return total
-        end
-
-        def get_num_matches_for_key(project1, project2, key)
-            matches = 0
-
-            if project1[key].kind_of?(Array)
-                matches = (project1[key] & project2[key]).length()
-            else
-                if project1[key] == project2[key]
-                    matches += 1
-                end
-            end
-
-            return matches
         end
 
         def slugify(title)
